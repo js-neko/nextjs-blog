d3a3bba5bd935c5dc9705f18020d9081686566f5
js-neko <nekofieldtest@gmail.com>
2025-11-15 11:42:11 +0000

    fix: add a blank line for better readability in the Home component

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+
       <main>
         <h1 className={styles.title}>
           Learn <Link href="/posts/first-post">Next.js!</Link>
